0b94658bfdd6a20962e3ea47c07b9be90e418853
JasmineCAicai <48902911+JasmineCAicai@users.noreply.github.com>
2021-09-18 11:43:56 +0800

    Update j_1085.cpp

--- j_1085.cpp
+++ j_1085.cpp
@@ -15,29 +15,23 @@ int main() {
         i++;
     }
     
-    vector<int> ans(n);
+    int ans = 0;
     
     sort(nums.begin(), nums.end());
     
     i = 0;
     while (i < n) {
         limit = nums[i] * p;
-        int total = n - 1;
-        while (total > i && nums[total] > limit) {
-            total--;
+        int total = ans + i;
+        while (total < n && nums[total] <= limit) {
+            ans = ans < (total - i + 1) ? total - i + 1 : ans;
+            total++;
         }
-        ans[i] = (total - i + 1);
+        
         i++;
     }
     
-    //sort(ans.begin(), ans.end(), [](const int a, const int b){ return a > b; });
-    long long max_value = -1;
-    for (i = 0; i < n; i++) {
-        max_value = max_value < ans[i] ? ans[i] : max_value;
-    }
-    
-    //cout << ans[0] << endl;
-    cout << max_value << endl;
+    cout << ans << endl;
     
     return 0;
 }
